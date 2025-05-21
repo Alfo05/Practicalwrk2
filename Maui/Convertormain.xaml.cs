afe12c05f97b875073e5928b00770d476a2b22de
Alfo05 <9300387@alumnos.ufv.es>
2025-05-21 17:02:06 +0200

    Adding the converter files

---
+++ Maui/Convertormain.xaml.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Maui
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Converter converter = new Converter();
+
+            int operation = converter.PrintOperations();
+
+            while (operation >= 1 && operation <= converter.GetNumberOperations())
+            {
+                Console.Clear();
+                Console.Write("Insert the number to convert: ");
+
+                string input = Console.ReadLine();
+
+                if (input != null && input != "")
+                {
+                    string output = converter.PerformConversion(
+                        operation,
+                        input);
+
+                    converter.PrintConversion(
+                        operation,
+                        input,
+                        output);
+                }
+
+                operation = converter.PrintOperations();
+            }
+        }
+    }
+}
